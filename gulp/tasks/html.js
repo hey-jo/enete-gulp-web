27032b588c5fa58d9b4addfee3aec6fb13a16fa1
Sergej <s.hartung@hws-energy.de>
2021-08-24 16:27:57 +0200

    24.08.2021

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   $.gulp.task('html', function () {
     return $.gulp.src('*.html')
-        .pipe($.gulp.dest('build'))
+        .pipe($.gulp.dest('build/html'))
         .on('end', $.browserSync.reload);
   });
 };
